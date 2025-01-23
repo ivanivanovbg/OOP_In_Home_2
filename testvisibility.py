0f27c1c2dd887e16fae4c503b929d861486abe3e
Ivan Ivanov <i.ivanov.bg@abv.bg>
2025-01-23 10:06:39 +0200

    Initial

--- testvisibility.py
+++ testvisibility.py
@@ -9,6 +9,7 @@ print("Attempt to change dc.__nv")
 dc.__nv = "This cannot be changed"
 print("dc.__nv : ID " + str(id(dc.__nv)) + " Value : " + dc.__nv)
 print("dc.nv : ID " + str(id(dc.nv)) + " Value : " + dc.nv)
+
 dc._v = "This is visible and already edited"
 print("dc.v : ID " + str(id(dc.v)) + " Value : " + dc.v)
 print("dc._v : ID " + str(id(dc._v)) + " Value : " + dc._v)
